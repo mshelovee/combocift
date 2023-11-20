bfacebdab24ed0b7c957203d587ffc694c179fa7
hilal <130781977+mshelovee@users.noreply.github.com>
2023-11-20 09:55:30 +0300

    Add files via upload

---
+++ combo box cift sayilar -96/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace combo_box_cift_sayilar__96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.cmbCift = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAktar.Location = new System.Drawing.Point(12, 349);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(156, 39);
+            this.btnAktar.TabIndex = 0;
+            this.btnAktar.Text = "Çift Sayıları Aktar";
+            this.btnAktar.UseVisualStyleBackColor = true;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.ItemHeight = 16;
+            this.lbSayilar.Location = new System.Drawing.Point(12, 12);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(156, 292);
+            this.lbSayilar.TabIndex = 1;
+            // 
+            // cmbCift
+            // 
+            this.cmbCift.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbCift.FormattingEnabled = true;
+            this.cmbCift.Location = new System.Drawing.Point(187, 12);
+            this.cmbCift.Name = "cmbCift";
+            this.cmbCift.Size = new System.Drawing.Size(156, 24);
+            this.cmbCift.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(368, 419);
+            this.Controls.Add(this.cmbCift);
+            this.Controls.Add(this.lbSayilar);
+            this.Controls.Add(this.btnAktar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAktar;
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.ComboBox cmbCift;
+    }
+}
+
---
+++ combo box cift sayilar -96/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combo_box_cift_sayilar__96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAktar_Click(object sender, EventArgs e)
+
+        {   
+            cmbCift.Items.Clear();
+            for (int i = 0; i < 20; i++)
+            {
+                int sayi = Convert.ToInt32(lbSayilar.Items[i]);
+                if (sayi % 2 == 0)
+                {
+                    cmbCift.Items.Add(sayi);
+                }
+            }
+
+        }   
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                int sayi = rnd.Next(0, 101);
+                lbSayilar.Items.Add(sayi);
+            }
+        }
+    }
+}
